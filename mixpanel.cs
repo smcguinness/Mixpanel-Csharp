d8edc28a1191ad97f611fedf68f1547f8c758686
Seth McGuinness <seth.mcguinness@yahoo.com>
2011-08-24 02:14:57 -0500

    initial commit

---
+++ mixpanel.cs
@@ -0,0 +1,30 @@
+public class Mixpanel
+    {
+        
+        private const string Host = "http://api.mixpanel.com/";
+        private string _token;
+        private string _mpEvent;
+        private Dictionary<string, string> _eventParams;
+        // This method that will be called when the thread is started
+        public Mixpanel(string token, string mpEvent, Dictionary<string, string> eventParams)
+        {
+            this._token = token;
+            this._mpEvent = mpEvent;
+            this._eventParams = eventParams;
+        }
+
+        public void Track()
+        {
+            this._eventParams.Add("token", this._token);
+            
+            var mpString = new {@event = this._mpEvent, properties = this._eventParams};
+            var mpJSON = mpString.ToJSON();
+
+            var encbuff = System.Text.Encoding.UTF8.GetBytes(mpJSON);
+            var encString = Convert.ToBase64String(encbuff);
+
+            var myReq = (HttpWebRequest)WebRequest.Create(Host + "track/?data=" + encString);
+            var myRes = myReq.GetResponse();
+
+        }
+    }
